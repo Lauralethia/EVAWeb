e5abccb36c3f4ab72c63f5e97cf01fab22aef47b
Lauralethia <31627878+Lauralethia@users.noreply.github.com>
2020-11-16 13:08:15 -0300

    upadted

--- model.py
+++ model.py
@@ -9,9 +9,9 @@ class InputForm(Form):
     Q2 = RadioField(
         label='2. Which methods do you base your main conclusions on?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Subject based, focused on biological/physiological process (neuroimaging, biological samples, etc)]'),
-                 (2,'Partially naturalistic [Subjective or cognitive performance scores, such as cognitive tests, free speech and natural language processing]'),
-                 (3,'Naturalistic real-world [Dependent on social interactions or context, such as classroom-based testing or social interaction outcomes]')],
+        choices=[(1,'CLR [Subject based, focused on biological/physiological process (neuroimaging, biological samples, etc)]'),
+                 (2,'NLRA [Subjective or cognitive performance scores, such as cognitive tests, free speech and natural language processing]'),
+                 (3,'NRWRA [Dependent on social interactions or context, such as classroom-based testing or social interaction outcomes]')],
         default=1,
         validators=[validators.InputRequired()])
     TQ2 = TextField(
@@ -24,9 +24,9 @@ class InputForm(Form):
     Q4 = RadioField(
         label='3. Describe the kind of stimuli used in your tasks. How naturalistic are your stimuli?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Static stimuli presented on a screen, many different trials using the same stimuli]'),
-                 (2,'Partially naturalistic [Dynamic stimuli that have context information, or meaning is dependent on the context (video, stories, etc)]'),
-                 (3,'Naturalistic real-world [Social interactions with peers]')],
+        choices=[(1,'CLR [Static stimuli presented on a screen, many different trials using the same stimuli]'),
+                 (2,'NLRA [Dynamic stimuli that have context information, or meaning is dependent on the context (video, stories, etc)]'),
+                 (3,'NRWRA [Social interactions with peers]')],
         default=1,
         validators=[validators.InputRequired()])   
     TQ4 = TextField(
@@ -36,9 +36,9 @@ class InputForm(Form):
     Q3 = RadioField(
         label='4. Describe your experimental approach.',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Measuring reaction times to faces presented on a screen]'),
-                 (2,'Partially naturalistic [Peer presence during a lab task]'),
-                 (3,'Naturalistic real-world [Social network analysis, in classroom behavior, EMA about social behavior]')],
+        choices=[(1,'CLR [Measuring reaction times to faces presented on a screen]'),
+                 (2,'NLRA [Peer presence during a lab task]'),
+                 (3,'NRWRA [Social network analysis, in classroom behavior, EMA about social behavior]')],
         default=1,
         validators=[validators.InputRequired()])
     TQ3 = TextField(
@@ -48,9 +48,9 @@ class InputForm(Form):
     Q5 = RadioField(
         label='5. Where is the testing taking place? (lab, home, classroom, public space e.g. museum or library)',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [In the lab or clinical facilities]'),
-                 (2,'Partially naturalistic [In the lab which looks naturalistic (e.g. in lab classroom), ambulatory patients or institutionalized subjects]'),
-                 (3,'Naturalistic real-world [In the classroom or the home with minimal researcher intervention on the environment]')],
+        choices=[(1,'CLR [In the lab or clinical facilities]'),
+                 (2,'NLRA [In the lab which looks naturalistic (e.g. in lab classroom), ambulatory patients or institutionalized subjects]'),
+                 (3,'NRWRA [In the classroom or the home with minimal researcher intervention on the environment]')],
         default=1,
         validators=[validators.InputRequired()])
     TQ5 = TextField(
@@ -60,9 +60,9 @@ class InputForm(Form):
     Q6 = RadioField(
         label='6. What is your study measuring?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [My study only measured in-lab task behavior and/or includes environmental variables only as covariates]'),
-                 (2,'Partially naturalistic [My study evaluates self / parent report on outside lab behavior as variables of interest]'),
-                 (3,'Naturalistic real-world [My study evaluates impact on real world grades/ test scores, incarceration, EMA or social network analysis]')],
+        choices=[(1,'CLR [My study only measured in-lab task behavior and/or includes environmental variables only as covariates]'),
+                 (2,'NLRA [My study evaluates self / parent report on outside lab behavior as variables of interest]'),
+                 (3,'NRWRA [My study evaluates impact on real world grades/ test scores, incarceration, EMA or social network analysis]')],
         default=1,
         validators=[validators.InputRequired()])
     TQ6 = TextField(
@@ -76,9 +76,9 @@ class InputForm(Form):
     Q4b = RadioField(
         label='3.2 If you have a second task, describe the kind of stimuli used in your tasks. How naturalistic are your stimuli?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Static stimuli presented on a screen, many different trials using the same stimuli]'),
-                 (2,'Partially naturalistic [Dynamic stimuli that have context information, or meaning is dependent on the context (video, stories, etc)]'),
-                 (3,'Naturalistic real-world [Interactive tasks with peers]'),
+        choices=[(1,'CLR [Static stimuli presented on a screen, many different trials using the same stimuli]'),
+                 (2,'NLRA [Dynamic stimuli that have context information, or meaning is dependent on the context (video, stories, etc)]'),
+                 (3,'NRWRA [Interactive tasks with peers]'),
                  (4,'No second task')],
 
         default=4,
@@ -90,9 +90,9 @@ class InputForm(Form):
     Q3b = RadioField(
         label='4.2 If you have a second task, describe your tasks.',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Measuring reaction times to faces presented on a screen]'),
-                 (2,'Partially naturalistic [Peer presence during a lab task]'),
-                 (3,'Naturalistic real-world [Social network analysis, in classroom behavior, EMA about social behavior]'),
+        choices=[(1,'CLR [Measuring reaction times to faces presented on a screen]'),
+                 (2,'NLRA [Peer presence during a lab task]'),
+                 (3,'NRWRA [Social network analysis, in classroom behavior, EMA about social behavior]'),
                  (4,'No second task')],
         default=4,
         validators=[validators.InputRequired()])
@@ -104,9 +104,9 @@ class InputForm(Form):
     Q5b = RadioField(
         label='5.2 If you have a second task, where is the testing taking place?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [In the lab or clinical facilities]'),
-                 (2,'Partially naturalistic [In the lab which looks naturalistic (e.g. in lab classroom), ambulatory patients or institutionalized subjects]'),
-                 (3,'Naturalistic real-world [In the classroom or the home with minimal researcher intervention on the environment]'),
+        choices=[(1,'CLR [In the lab or clinical facilities]'),
+                 (2,'NLRA [In the lab which looks naturalistic (e.g. in lab classroom), ambulatory patients or institutionalized subjects]'),
+                 (3,'NRWRA [In the classroom or the home with minimal researcher intervention on the environment]'),
                  (4,'No second task')],
 
         default=4,
@@ -118,9 +118,9 @@ class InputForm(Form):
     Q6b = RadioField(
         label='6.2 If you have a second task, what is it measuring?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [My study only measured in-lab task behavior and/or includes environmental variables only as covariates]'),
-                 (2,'Partially naturalistic [My study evaluates self / parent report on outside lab behavior as variables of interest]'),
-                 (3,'Naturalistic real-world [My study evaluates impact on real world grades/ test scores, incarceration, EMA or social network analysis]'),
+        choices=[(1,'CLR [My study only measured in-lab task behavior and/or includes environmental variables only as covariates]'),
+                 (2,'NLRA [My study evaluates self / parent report on outside lab behavior as variables of interest]'),
+                 (3,'NRWRA [My study evaluates impact on real world grades/ test scores, incarceration, EMA or social network analysis]'),
                  (4,'No second task')],
         default=4,
         validators=[validators.InputRequired()])
@@ -134,9 +134,9 @@ class InputForm(Form):
     Q4c = RadioField(
         label='3.3 If you have a second task, describe the kind of stimuli used in your tasks. How naturalistic are your stimuli?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Static stimuli presented on a screen, many different trials using the same stimuli]'),
-                 (2,'Partially naturalistic [Dynamic stimuli that have context information, or meaning is dependent on the context (video, stories, etc)]'),
-                 (3,'Naturalistic real-world [Interactive tasks with peers]'),
+        choices=[(1,'CLR [Static stimuli presented on a screen, many different trials using the same stimuli]'),
+                 (2,'NLRA [Dynamic stimuli that have context information, or meaning is dependent on the context (video, stories, etc)]'),
+                 (3,'NRWRA [Interactive tasks with peers]'),
                  (4,'No third task')],
 
         default=4,
@@ -145,9 +145,9 @@ class InputForm(Form):
     Q3c = RadioField(
         label='4.3 If you have a third task, describe your tasks.',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Measuring reaction times to faces presented on a screen]'),
-                 (2,'Partially naturalistic [Peer presence during a lab task]'),
-                 (3,'Naturalistic real-world [Social network analysis, in classroom behavior, EMA about social behavior]'),
+        choices=[(1,'CLR [Measuring reaction times to faces presented on a screen]'),
+                 (2,'NLRA [Peer presence during a lab task]'),
+                 (3,'NRWRA [Social network analysis, in classroom behavior, EMA about social behavior]'),
                  (4,'No third task')],
         default=4,
         validators=[validators.InputRequired()])
@@ -162,9 +162,9 @@ class InputForm(Form):
     Q5c = RadioField(
         label='5.2 If you have a third task, where is the testing taking place?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [In the lab or clinical facilities]'),
-                 (2,'Partially naturalistic [In the lab which looks naturalistic (e.g. in lab classroom), ambulatory patients or institutionalized subjects]'),
-                 (3,'Naturalistic real-world [In the classroom or the home with minimal researcher intervention on the environment]'),
+        choices=[(1,'CLR [In the lab or clinical facilities]'),
+                 (2,'NLRA [In the lab which looks naturalistic (e.g. in lab classroom), ambulatory patients or institutionalized subjects]'),
+                 (3,'NRWRA [In the classroom or the home with minimal researcher intervention on the environment]'),
                  (4,'No third task')],
 
         default=4,
@@ -176,9 +176,9 @@ class InputForm(Form):
     Q6c = RadioField(
         label='6.3 If you have a third task, what is it measuring?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [My study only measured in-lab task behavior and/or includes environmental variables only as covariates]'),
-                 (2,'Partially naturalistic [My study evaluates self / parent report on outside lab behavior as variables of interest]'),
-                 (3,'Naturalistic real-world [My study evaluates impact on real world grades/ test scores, incarceration, EMA or social network analysis]'),
+        choices=[(1,'CLR [My study only measured in-lab task behavior and/or includes environmental variables only as covariates]'),
+                 (2,'NLRA [My study evaluates self / parent report on outside lab behavior as variables of interest]'),
+                 (3,'NRWRA [My study evaluates impact on real world grades/ test scores, incarceration, EMA or social network analysis]'),
                  (4,'No third task')],
         default=4,
         validators=[validators.InputRequired()])
@@ -190,9 +190,9 @@ class InputForm(Form):
     Q7 = RadioField(
         label='7. How does sample recruitment take place? Is your sample representative of your region? How important do you think this is for your particular question?',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [Convenience sample (e.g., students)]'),
-                 (2,'Partially naturalistic [Community-based recruitment]'),
-                 (3,'Naturalistic real-world [Nationally representative sample by demographics and socio-economic status]')],
+        choices=[(1,'CLR [Convenience sample (e.g., students)]'),
+                 (2,'NLRA [Community-based recruitment]'),
+                 (3,'NRWRA [Nationally representative sample by demographics and socio-economic status]')],
         default=1,
         validators=[validators.InputRequired()])
     TQ7 = TextField(
@@ -202,9 +202,9 @@ class InputForm(Form):
     Q8 = RadioField(
         label='8. Are non-research stakeholders involved? (teachers, parents, institutions, clinicians)',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [No involvement from other stakeholders besides facilitating the sample]'),
-                 (2,'Partially naturalistic [Involvement in result interpretation / writing]'),
-                 (3,'Naturalistic real-world [Involvement in study design / implementation]')],
+        choices=[(1,'CLR [No involvement from other stakeholders besides facilitating the sample]'),
+                 (2,'NLRA [Involvement in result interpretation / writing]'),
+                 (3,'NRWRA [Involvement in study design / implementation]')],
         default=1,
         validators=[validators.InputRequired()])
     TQ8 = TextField(
@@ -224,9 +224,9 @@ class InputForm(Form):
     Q11 = RadioField(
         label='11. If R9 is yes, please indicate where your intervention fits in best. ',
         coerce=int,
-        choices=[(1,'Classic laboratory-based [***]'),
-                 (2,'Partially naturalistic [***]'),
-                 (3,'Naturalistic real-world [***]'),
+        choices=[(1,'CLR [***]'),
+                 (2,'NLRA [***]'),
+                 (3,'NRWRA [***]'),
                  (4,'No intervention component')],
         default=4,
         validators=[validators.InputRequired()])
@@ -236,5 +236,5 @@ class InputForm(Form):
 
     
     TQF = TextAreaField(
-        label='12. Lastly, please indicate in which category (classic laboratory-based, partially naturalistic, and fully naturalistic) you see your research fits best and state the reasons.',
+        label='12. Lastly, please indicate in which category (CLR, NLRA, and NRWRA) you see your research fits best and state the reasons.',
         default='No comment')
